efe8e5ea3facbc7ab5ab4a03302f6b1f08cdbc3b
Ikram Eddahby <ikram22eddahby@gmail.com>
2025-02-10 16:46:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ function loginEffect() {
     }, 1000);
 }
 
-// تأثير الإطار المتحرك عند تحريك الماوس
+
 const loginBox = document.getElementById("loginBox");
 document.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;  // اجعل التأثير أقوى
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 10; // اجعل التأثير أقوى
-    loginBox.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg) scale(1.1)`; // إضافة تكبير عند الاقتراب
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;  // 
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10; // 
+    loginBox.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg) scale(1.1)`; // 
 });
 
-// عند خروج الماوس يعود الإطار لوضعه العادي
+
 document.addEventListener("mouseleave", () => {
     loginBox.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
 });
